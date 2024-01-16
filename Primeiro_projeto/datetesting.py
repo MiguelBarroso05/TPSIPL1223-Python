6a0bd1136c5e7c880f218234b1746b2dfc7b303e
Miguel Barroso <miguelreisbarroso@gmail.com>
2024-01-16 17:15:10 +0000

    update 16/01

---
+++ Primeiro_projeto/datetesting.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+minf = datetime.now().time().minute +1
+
+c = 0
+while True:
+    #print(datetime.now().time().second)
+
+    if datetime.now().time().second == 59:
+        c += 1
+
+    if datetime.now().time().minute >= minf:
+        break
+
+print(c)
